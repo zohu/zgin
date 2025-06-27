dc2c47abb6ce28b4250db88ed3e057e8c87f9b52
zohu <38601989@qq.com>
2025-06-27 17:06:13 +0800

    feat: binding

--- zmath/util.go
+++ zmath/util.go
@@ -6,8 +6,8 @@ import (
 
 type Number interface {
 	~float32 | ~float64 |
-	~int | ~int8 | ~int16 | ~int32 | ~int64 |
-	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64
+		~int | ~int8 | ~int16 | ~int32 | ~int64 |
+		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64
 }
 
 func ToInt64(v string) int64 {
