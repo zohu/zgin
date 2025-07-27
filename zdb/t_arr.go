0ab7970d1896c3689337318240f45134bd331bdf
zohu <38601989@qq.com>
2025-07-27 22:14:00 +0800

    fix: ws LoadFunc

--- zdb/t_arr.go
+++ zdb/t_arr.go
@@ -13,28 +13,28 @@ type StringArray struct {
 	pq.StringArray
 }
 
-func NewStringArray(arr []string) StringArray {
-	return StringArray{arr}
+func NewStringArray(arr []string) *StringArray {
+	return &StringArray{arr}
 }
 
 // gorm 支持
 
-func (a StringArray) GormDataType() string {
+func (a *StringArray) GormDataType() string {
 	return "text[]"
 }
-func (a StringArray) String() string {
+func (a *StringArray) String() string {
 	return strings.Join(a.StringArray, ",")
 }
-func (a StringArray) Scan(src interface{}) error {
+func (a *StringArray) Scan(src interface{}) error {
 	return a.StringArray.Scan(src)
 }
-func (a StringArray) Value() (driver.Value, error) {
+func (a *StringArray) Value() (driver.Value, error) {
 	return a.StringArray.Value()
 }
 
 // JSON 支持
 
-func (a StringArray) UnmarshalJSON(data []byte) error {
+func (a *StringArray) UnmarshalJSON(data []byte) error {
 	var strs []string
 	if err := sonic.Unmarshal(data, &strs); err != nil {
 		return err
@@ -43,26 +43,26 @@ func (a StringArray) UnmarshalJSON(data []byte) error {
 	return nil
 }
 
-func (a StringArray) MarshalJSON() ([]byte, error) {
+func (a *StringArray) MarshalJSON() ([]byte, error) {
 	return sonic.Marshal([]string(a.StringArray))
 }
-func (a StringArray) Contains(v string) bool {
+func (a *StringArray) Contains(v string) bool {
 	return slices.Contains(a.StringArray, v)
 }
-func (a StringArray) Append(v string) StringArray {
+func (a *StringArray) Append(v string) *StringArray {
 	if v == "" {
 		return a
 	}
 	a.StringArray = append(a.StringArray, v)
 	return a
 }
-func (a StringArray) AppendOnce(v string) StringArray {
+func (a *StringArray) AppendOnce(v string) *StringArray {
 	if a.Contains(v) {
 		return a
 	}
 	return a.Append(v)
 }
-func (a StringArray) Equal(b StringArray) bool {
+func (a *StringArray) Equal(b *StringArray) bool {
 	return slices.Equal(a.StringArray, b.StringArray)
 }
 
@@ -71,7 +71,7 @@ func (a StringArray) Equal(b StringArray) bool {
 // @receiver a
 // @param b
 // @return bool
-func (a StringArray) ItemEqual(b StringArray) bool {
+func (a *StringArray) ItemEqual(b *StringArray) bool {
 	if len(a.StringArray) != len(b.StringArray) {
 		return false
 	}
